a2ee7972507d20e62e5e4454dcbf1add7aa37306
sjodavid94 <97950403+sjodavid94@users.noreply.github.com>
2025-05-13 18:23:09 +0200

    Update server.js
    - bancontact added?

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({
         }
 
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
+            payment_method_types: ["card", "bancontact"],
             line_items: [
                 {
                     price_data: {
